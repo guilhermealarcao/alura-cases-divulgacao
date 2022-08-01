139c4bebb08738915773cc59a5fe4ce7afe1a85d
Guilherme <guilhermealarcao@gmail.com>
2022-08-01 17:40:55 -0300

    aula 3.2

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Link from "../src/components/Link";
-
+import Title from "../src/components/Title";
 
 export default function HomePage(){
     return (
         <div>
+            <Title as="h1">TEste</Title>
             <h1>Alura cases - Home Page</h1>
             <Link href="/faq">
                 Ir para FAQ
---
+++ src/components/Title/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Title({ children, as }) {
+    const Tag = as;
+    return (    
+        <>
+            <Tag>{children}</Tag>
+            <style jsx>
+                {`${Tag}{
+                    color:red;
+                }`}
+            </style>
+        </>
+    );
+}
