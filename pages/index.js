ffb5921d7e628f157a41a0bbf3838a477560169d
Guilherme <guilhermealarcao@gmail.com>
2022-08-01 18:53:46 -0300

    aula 3.5

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,8 @@
-import Link from "../src/components/Link";
-import Title from "../src/components/Title";
+import HomeScreen from "../src/components/screens/HomeScreen";
 
-export default function HomePage(){
+
+export default function HomePage() {
     return (
-        <div>
-            <Title as="h1">TEste</Title>
-            <h1>Alura cases - Home Page</h1>
-            <Link href="/faq">
-                Ir para FAQ
-            </Link>
-        </div>
+        <HomeScreen />
     )
 }
