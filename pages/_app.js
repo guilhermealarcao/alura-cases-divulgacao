8477da8e0e267a88b7dd23ad4903b0f74306a9ba
Guilherme <guilhermealarcao@gmail.com>
2022-08-01 23:23:28 -0300

    arrumando novamente o build

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin={true.toString()} />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet" />
       </Head>
       <GlobalStyle />
