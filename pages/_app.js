16b7afa60460fe536018ca491a93e8e34c5ac2e9
Guilherme <guilhermealarcao@gmail.com>
2022-08-01 23:26:23 -0300

    adicionando font de forma correta

--- pages/_app.js
+++ pages/_app.js
@@ -5,11 +5,6 @@ import Head from 'next/head';
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin={true.toString()} />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet" />
-      </Head>
       <GlobalStyle />
       <Component {...pageProps} />
     </>
